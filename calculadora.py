18f2d94ecf44651d2e6d2917de5be4b926ae20ff
JRamirez1205 <julydr2@gmail.com>
2025-06-29 13:40:06 -0500

    first commit

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+print("Esta es una calculadora")
+print("Ingrese el valor a")
+a = int(input())
+print("ingrese b")
+b = int(input())
+c = a+b
+e = a-b
+print("El resultado es igual de la suma es :", c)
+print("El resultado de la resta es:", e)
